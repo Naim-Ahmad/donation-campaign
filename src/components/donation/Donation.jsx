6f23438e50f8ebcde8a4e1bdd1ee5e40b2c96007
Naim Ahmad <naimahmad201103@gmail.com>
2023-09-25 21:17:04 +0600

    donation page finished with all functionality

--- src/components/donation/Donation.jsx
+++ src/components/donation/Donation.jsx
@@ -1,7 +1,38 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
+import { getDonation } from '../../utilities/localstorage'
+import Card from './Card'
 
 export default function Donation() {
+  const [data, setData] = useState([])
+  const [slice, setSlice] = useState(4)
+  
+  const handleClick = () => {
+    setSlice(data.length)
+  }
+  useEffect(() => {
+    fetch('data.json')
+      .then(res => res.json())
+      .then(data => {
+        const allId = getDonation()
+        const newData = []
+        for (const id of allId) {
+          const findData = data.find(item => item.id == id)
+          if (findData) {
+            newData.push(findData)
+          }
+        }
+        setData(newData)
+      })
+  }, [])
+  
   return (
-    <div>Donation</div>
+    <section>
+      <div className='grid grid-cols-2 gap-8 my-10'>
+        {data.slice(0, slice).map(item => <Card key={item.id} data={item}/>)}
+      </div>
+      <div className='text-center mt-8'>
+        {data.length > 4 && data.length !== slice && <button onClick={handleClick} className='btn text-white hover:bg-[#1d6b42] bg-[#009444] '>Show All</button>}
+      </div>
+    </section>
   )
 }
